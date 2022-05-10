d247b49ec324ee7cac986b6cb92e05fa221aa1cd
Nhlanhla Mbokazi <nhlanhla.mbokazi@dadevs.co.za>
2022-05-10 11:35:43 +0200

    About

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
   {
     path: 'nav', component: NavComponent
+  },
+  {
+    path: 'about', component: AboutComponent
   }
 ];
 
